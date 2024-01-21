72829004ccbfdd8c7a94f9d265a75523bec892e3
Andrii Rodzyk <rodzyk@programmer.net>
2024-01-21 10:46:12 +0200

    fix: port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 corsOptions = {
     // origin: 'http://192.168.43.243:4200',
