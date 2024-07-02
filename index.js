1862930b10f1073decb6475de4a67597ce71ccec
Andrii Rodzyk <rodzyk@programmer.net>
2024-07-02 11:59:43 +0300

    add validateStatus

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ async function getRedirectUrl(url) {
     try {
       const response = await fetch(url, {
         redirect: 'manual', // Забороняємо автоматичне слідування за редіректами
+        validateStatus: status => status >= 200 && status < 400,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
