f83b9f83df4cf60e7d9fb02d4e0c05412a859e64
Andrii Rodzyk <rodzyk@programmer.net>
2026-06-24 16:46:08 +0300

    change User-Agent

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ app.get('/api', async (req, res) => {
         const authorizationHeader = req.headers.authorization
 
         const headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+            // 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+            'User-Agent': 'ChytankaProxy/1.0',
             'Referer': ref,
             "Authorization": authorizationHeader
         }
